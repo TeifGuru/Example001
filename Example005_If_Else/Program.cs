e390b1e1f85a90a33852c7914ffbe97381f9c667
TeifGuru <TeifGuru@gmail.com>
2023-07-08 18:40:21 +0300

    If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username=Console.ReadLine();
+
+if(username=="Маша")
+{
+    Console.WriteLine("Ура, это Маша");
+
+}
+else
+{
+    Console.WriteLine("Привет");
+    Console.WriteLine(username);
+}
